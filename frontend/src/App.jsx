b2680546487acbadfa8ec5fe850ea3708c569d57
Devbharath2k <devbharath21k@gmail.com>
2025-01-04 20:35:44 +0530

    frontend-version

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/footer' element={<Footer />} />
+      </Routes>
+    </div>
   )
 }
 
